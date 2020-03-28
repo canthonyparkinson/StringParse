c926a416fbad52f93253d652eb851d72dbe41558
parkinan <canthonyparkinson@shaw.ca>
2020-03-28 10:51:47 -0600

    Test for DateTime and TimeSpan

--- StringParseTests/TestDateTime.cs
+++ StringParseTests/TestDateTime.cs
@@ -1,14 +1,89 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ca.canthonyparkinson.StringParse;
+using System.Globalization;
 
 namespace StringParseTests
 {
     [TestClass]
     public class TestDateTime
     {
+        private static CultureInfo enUS = new CultureInfo("en-US");
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestValid1()
         {
+            AssertValid("2020/03/28 23:43:53".ParseDateTime(), new DateTime(2020,3,28,23,43,53));
+            AssertValid("03/28/2020 23:43:53".ParseDateTime(), new DateTime(2020, 3, 28, 23, 43, 53));
+            AssertValid("2020/03/28".ParseDateTime(), new DateTime(2020, 3, 28));
         }
+        [TestMethod]
+        public void TestValid3()
+        {
+            AssertValid("03/28/2020 23:43:53".ParseDateTime(enUS, DateTimeStyles.None), new DateTime(2020, 3, 28, 23, 43, 53), DateTimeKind.Unspecified);
+            AssertValid("03/28/2020 23:43:53".ParseDateTime(enUS, DateTimeStyles.AssumeLocal), new DateTime(2020, 3, 28, 23, 43, 53), DateTimeKind.Local);
+            AssertValid("03/28/2020 23:43:53 -05:00".ParseDateTime(enUS, DateTimeStyles.AdjustToUniversal), new DateTime(2020, 3, 29, 4, 43, 53), DateTimeKind.Utc);
+        }
+        [TestMethod]
+        public void TestValidExactSingle()
+        {
+            AssertValid("03/28/2020 23:43:53".ParseDateTimeExact("MM/dd/yyyy HH:mm:ss", enUS, DateTimeStyles.None), new DateTime(2020, 3, 28, 23, 43, 53), DateTimeKind.Unspecified);
+            AssertValid("03/28/2020 23:43:53".ParseDateTimeExact("MM/dd/yyyy HH:mm:ss", enUS, DateTimeStyles.AssumeLocal), new DateTime(2020, 3, 28, 23, 43, 53), DateTimeKind.Local);
+            AssertValid("03/28/2020 23:43:53 -05:00".ParseDateTimeExact("MM/dd/yyyy HH:mm:ss zzz", enUS, DateTimeStyles.AdjustToUniversal), new DateTime(2020, 3, 29, 4, 43, 53), DateTimeKind.Utc);
+        }
+        [TestMethod]
+        public void TestValidExactArray()
+        {
+            AssertValid("03/28/2020 23:43:53".ParseDateTimeExact(new string[] { "MM/dd/yyyy HH:mm:ss" }, enUS, DateTimeStyles.None), new DateTime(2020, 3, 28, 23, 43, 53), DateTimeKind.Unspecified);
+            AssertValid("03/28/2020 23:43:53".ParseDateTimeExact(new string[] { "MM/dd/yyyy HH:mm:ss" }, enUS, DateTimeStyles.AssumeLocal), new DateTime(2020, 3, 28, 23, 43, 53), DateTimeKind.Local);
+            AssertValid("03/28/2020 23:43:53 -05:00".ParseDateTimeExact(new string[] { "MM/dd/yyyy HH:mm:ss zzz" }, enUS, DateTimeStyles.AdjustToUniversal), new DateTime(2020, 3, 29, 4, 43, 53), DateTimeKind.Utc);
+        }
+        [TestMethod]
+        public void TestInvalid1()
+        {
+            Assert.IsFalse("".ParseDateTime().HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseDateTime().HasValue);
+            Assert.IsFalse("today".ParseDateTime().HasValue);
+        }
+        [TestMethod]
+        public void TestInvalid3()
+        {
+            Assert.IsFalse("".ParseDateTime(enUS, DateTimeStyles.None).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseDateTime(enUS, DateTimeStyles.None).HasValue);
+            Assert.IsFalse("today".ParseDateTime(enUS, DateTimeStyles.None).HasValue);
+        }
+        [TestMethod]
+        public void TestInvalidExactSingle()
+        {
+            Assert.IsFalse("".ParseDateTimeExact("MM/dd/yyyy HH:mm:ss", enUS, DateTimeStyles.None).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseDateTimeExact("MM/dd/yyyy HH:mm:ss", enUS, DateTimeStyles.None).HasValue);
+            Assert.IsFalse("today".ParseDateTimeExact("MM/dd/yyyy HH:mm:ss", enUS, DateTimeStyles.None).HasValue);
+        }
+        [TestMethod]
+        public void TestInvalidExactArray()
+        {
+            Assert.IsFalse("".ParseDateTimeExact(new string[] { "MM/dd/yyyy HH:mm:ss" }, enUS, DateTimeStyles.None).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseDateTimeExact(new string[] { "MM/dd/yyyy HH:mm:ss" }, enUS, DateTimeStyles.None).HasValue);
+            Assert.IsFalse("today".ParseDateTimeExact(new string[] { "MM/dd/yyyy HH:mm:ss" }, enUS, DateTimeStyles.None).HasValue);
+        }
+
+        private void AssertValid(DateTime? rslt, DateTime expected)
+        {
+            Assert.IsTrue(rslt.HasValue);
+            Assert.AreEqual(expected, rslt.Value);
+        }
+
+        private void AssertValid(DateTime? rslt, DateTime expected, DateTimeKind expectedKind)
+        {
+            Assert.IsTrue(rslt.HasValue);
+            Assert.AreEqual(expected, rslt.Value);
+            Assert.IsTrue(rslt.HasValue);
+            Assert.AreEqual(expectedKind, rslt.Value.Kind);
+        }
+
     }
 }
--- StringParseTests/TestTimeSpan.cs
+++ StringParseTests/TestTimeSpan.cs
@@ -1,14 +1,108 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ca.canthonyparkinson.StringParse;
+using System.Globalization;
 
 namespace StringParseTests
 {
     [TestClass]
     public class TestTimeSpan
     {
+        private static CultureInfo enUS = new CultureInfo("en-US");
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestValid1()
         {
+            AssertValid("23:43:53".ParseTimeSpan(), new TimeSpan(23, 43, 53));
+            AssertValid("23:43:53.654".ParseTimeSpan(), new TimeSpan(0, 23, 43, 53, 654));
         }
+
+        [TestMethod]
+        public void TestValid2()
+        {
+            AssertValid("23:43:53".ParseTimeSpan(enUS), new TimeSpan(23, 43, 53));
+            AssertValid("23:43:53.654".ParseTimeSpan(enUS), new TimeSpan(0, 23, 43, 53, 654));
+        }
+        [TestMethod]
+        public void TestValidExactSingle3()
+        {
+            AssertValid("23:43:53".ParseTimeSpanExact("g",enUS), new TimeSpan(23, 43, 53));
+            AssertValid("23:43:53.654".ParseTimeSpanExact("g", enUS), new TimeSpan(0, 23, 43, 53, 654));
+        }
+        [TestMethod]
+        public void TestValidExactArray3()
+        {
+            AssertValid("23:43:53".ParseTimeSpanExact(new string[] { "g" }, enUS), new TimeSpan(23, 43, 53));
+            AssertValid("23:43:53.654".ParseTimeSpanExact(new string[] { "g" }, enUS), new TimeSpan(0, 23, 43, 53, 654));
+        }
+        [TestMethod]
+        public void TestValidExactSingle4()
+        {
+            AssertValid("23:43:53".ParseTimeSpanExact("g", enUS, TimeSpanStyles.None), new TimeSpan(23, 43, 53));
+            AssertValid("23:43:53.654".ParseTimeSpanExact("g", enUS, TimeSpanStyles.None), new TimeSpan(0, 23, 43, 53, 654));
+        }
+        [TestMethod]
+        public void TestValidExactArray4()
+        {
+            AssertValid("23:43:53".ParseTimeSpanExact(new string[] { "g" }, enUS, TimeSpanStyles.None), new TimeSpan(23, 43, 53));
+            AssertValid("23:43:53.654".ParseTimeSpanExact(new string[] { "g" }, enUS, TimeSpanStyles.None), new TimeSpan(0, 23, 43, 53, 654));
+        }
+
+        [TestMethod]
+        public void TestInvalid1()
+        {
+            Assert.IsFalse("".ParseTimeSpan().HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseTimeSpan().HasValue);
+            Assert.IsFalse("One Hour".ParseTimeSpan().HasValue);
+        }
+
+        [TestMethod]
+        public void TestInvalid2()
+        {
+            Assert.IsFalse("".ParseTimeSpan(enUS).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseTimeSpan(enUS).HasValue);
+            Assert.IsFalse("One Hour".ParseTimeSpan(enUS).HasValue);
+        }
+        [TestMethod]
+        public void TestInvalidExactSingle3()
+        {
+            Assert.IsFalse("".ParseTimeSpanExact("g", enUS).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseTimeSpanExact("g", enUS).HasValue);
+            Assert.IsFalse("One Hour".ParseTimeSpanExact("g", enUS).HasValue);
+        }
+        [TestMethod]
+        public void TestInvalidExactArray3()
+        {
+            Assert.IsFalse("".ParseTimeSpanExact(new string[] { "g" }, enUS).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseTimeSpanExact(new string[] { "g" }, enUS).HasValue);
+            Assert.IsFalse("One Hour".ParseTimeSpanExact(new string[] { "g" }, enUS).HasValue);
+        }
+        [TestMethod]
+        public void TestInvalidExactSingle4()
+        {
+            Assert.IsFalse("".ParseTimeSpanExact("g", enUS, TimeSpanStyles.None).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseTimeSpanExact("g", enUS, TimeSpanStyles.None).HasValue);
+            Assert.IsFalse("One Hour".ParseTimeSpanExact("g", enUS, TimeSpanStyles.None).HasValue);
+        }
+        [TestMethod]
+        public void TestInvalidExactArray4()
+        {
+            Assert.IsFalse("".ParseTimeSpanExact(new string[] { "g" }, enUS, TimeSpanStyles.None).HasValue);
+            String s = null;
+            Assert.IsFalse(s.ParseTimeSpanExact(new string[] { "g" }, enUS, TimeSpanStyles.None).HasValue);
+            Assert.IsFalse("One Hour".ParseTimeSpanExact(new string[] { "g" }, enUS, TimeSpanStyles.None).HasValue);
+        }
+
+        private void AssertValid(TimeSpan? rslt, TimeSpan expected)
+        {
+            Assert.IsTrue(rslt.HasValue);
+            Assert.AreEqual(expected, rslt.Value);
+        }
+
     }
 }
